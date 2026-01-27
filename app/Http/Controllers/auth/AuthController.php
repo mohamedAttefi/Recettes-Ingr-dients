06b627cbb41c15d3bc785f86c47607f5e97ff44e
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-27 16:37:18 +0100

    add authentification controller

---
+++ app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showRegisterForm()
+    {
+        return view("auth.register");
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return redirect()->route('login');
+    }
+
+    public function showLoginForm(){
+        return view("auth.login");
+    }
+public function login(Request $request)
+{
+    $user = User::where('email', $request->email)->first();
+
+    if ($user && Hash::check($request->password, $user->password)) {
+        Auth::login($user);
+        return redirect()->route('dashboard');
+    }
+    return redirect()->back()->with("error", "invalid informations");
+
+}
+}
