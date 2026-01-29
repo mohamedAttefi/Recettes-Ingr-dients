6ca1a1fcfe97ff68caa4c246dd3396ae2b34bfbe
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-29 11:04:40 +0100

    update web routers

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,12 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [UserController::class, "index"])->name('dashboard');
-
-Route::get("/myRecipe", [RecipeController::class, "showMyRecipe"])->name("myRecipe");
-Route::get("/addRecipe", [RecipeController::class, "showAddRecipeForm"])->name("addRecipe");
+Route::get("/recipe/myRecipe", [RecipeController::class, "showMyRecipe"])->name("myRecipe");
+Route::get("/recipe/addRecipe", [RecipeController::class, "showAddRecipeForm"])->name("addRecipe.form");
+Route::get("/recipe/showRecipe", [RecipeController::class, "showRecipe"])->name("showRecipe");
+Route::get("/recipe/editRecipe", [RecipeController::class, "showEditRecipe"])->name("showEditRecipeForm");
+Route::post("/recipe/editRecipe", [RecipeController::class, "update"])->name("update");
+Route::post("/recipe/addRecipe", [RecipeController::class, "store"])->name("addRecipe");
+Route::get("/recipe/All", [RecipeController::class, "index"])->name("allRecipes");
 
 require __DIR__ . '/auth.php';
