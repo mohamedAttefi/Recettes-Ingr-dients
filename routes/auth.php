d8071771081620ef8dd748bd1e1b88318924a71a
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-29 11:04:22 +0100

    update auth routers

--- routes/auth.php
+++ routes/auth.php
@@ -7,5 +7,4 @@ Route::post("login", [AuthController::class, "login"])->name("login");
 
 Route::get('register', [AuthController::class, "showRegisterForm"])->name("register.form");
 Route::get('login', [AuthController::class, "showLoginForm"])->name("login.form");
-
 Route::post('register', [AuthController::class, "register"])->name("register");
