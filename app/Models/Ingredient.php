012a873ed5f2a1f35fad5f2ef44592a86ca6e2a7
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-29 11:07:37 +0100

    update ingredient model

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Ingredient extends Model
 {
      protected $table = "ingredients";
-    protected $fillable = ["name"];
+    protected $fillable = ["nom", "recipe_id"];
 
     public function recipe(){
-        return $this->belongsToMany(Recipe::class, "recipe_ingredient")->using(IngredientRecipe::class);
+        return $this->belongsTo(Recipe::class, "recipe_ingredient");
     }
 
 }
